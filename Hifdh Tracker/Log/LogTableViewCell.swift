aa6b05b733c7fac0eaefe038165e0ba18dbdd61f
Owais Quadri <owaisquadri01@gmail.com>
2023-07-10 01:35:02 -0400

    create basic UI for log screen

---
+++ Hifdh Tracker/Log/LogTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LogTableViewCell.swift
+//  Hifdh Tracker
+//
+//  Created by Owais on 2023-07-10.
+//
+
+import UIKit
+
+class LogTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var memorySwitch: UISwitch!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Hifdh Tracker/Log/LogTableViewController.swift
@@ -0,0 +1,66 @@
+//
+//  LogTableViewController.swift
+//  Hifdh Tracker
+//
+//  Created by Owais on 2023-07-09.
+//
+
+import UIKit
+
+class LogTableViewController: UITableViewController {
+    let logs = [ 601, 602, 603, 604 ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3 + logs.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+            case 0:
+                if let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell") as? DateTableViewCell {
+                    cell.titleLabel.text = "Start Date:"
+                    
+                    //set the date from persisting storage
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "MM/dd/yyyy"
+                    let startDate = dateFormatter.date(from: "05/01/2022")
+                    cell.datePicker.date = startDate ?? Date()
+                    
+                    return cell
+                }
+            case 1:
+                if let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell") as? DateTableViewCell {
+                    cell.titleLabel.text = "Today's Date:"
+                    cell.datePicker.isEnabled = false
+                    return cell
+                }
+            case 2:
+                if let configCell = tableView.dequeueReusableCell(withIdentifier: "configCell") as? ConfigurationTableViewCell {
+                    return configCell
+                }
+            default:
+                if let logCell = tableView.dequeueReusableCell(withIdentifier: "logCell") as? LogTableViewCell {
+                    logCell.titleLabel.text = String(logs[indexPath.row - 3])
+                    return logCell
+                }
+        }
+        return UITableViewCell()
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
