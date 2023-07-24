324bd0b004e1c5c8d09d6f07689459b671840556
Owais Quadri <owaisquadri01@gmail.com>
2023-07-23 21:23:38 -0400

    #18 / #19 : - integer format - date format

--- Hifdh Tracker/Util/Format+Util.swift
+++ Hifdh Tracker/Util/Format+Util.swift
@@ -9,6 +9,11 @@ import Foundation
 
 extension NumberFormatter {
     
+    static func integer(_ number: Double) -> String? {
+        let integerFormat = NumberFormatter()
+        integerFormat.numberStyle = .none
+        return integerFormat.string(from: NSNumber(floatLiteral: number))
+    }
     static func twoDecimals(_ number: Double) -> String? {
         let twoDecimalFormat = NumberFormatter()
         twoDecimalFormat.maximumFractionDigits = 2
@@ -22,5 +27,13 @@ extension NumberFormatter {
         percentFormat.maximumFractionDigits = 1
         percentFormat.minimumFractionDigits = 1
         return percentFormat.string(from: NSNumber(floatLiteral: number))
+    }
+}
+
+extension DateFormatter {
+    static func mmmDDyyyy(_ date: Date) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM/dd/yyyy"
+        return dateFormatter.string(from: date)
     }
 }
