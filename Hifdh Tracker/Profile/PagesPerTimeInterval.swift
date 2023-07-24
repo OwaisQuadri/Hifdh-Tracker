7ce9c1917a8f54c4f293e172762f614a7b41f50e
Owais Quadri <owaisquadri01@gmail.com>
2023-07-23 23:46:49 -0400

    #51: chart columns are selectable (#53)
    - QOL changes and chart Enhancements

--- Hifdh Tracker/Profile/PagesPerTimeInterval.swift
+++ Hifdh Tracker/Profile/PagesPerTimeInterval.swift
@@ -5,8 +5,6 @@
 //  Created by Owais on 2023-07-22.
 //
 
-import SwiftUI
-import Charts
 import Foundation
 
 // MARK: Chart's Model
@@ -37,61 +35,3 @@ struct PagesPerMonth: Identifiable {
     }
     
 }
-
-// MARK: SwiftUI View
-
-struct MemorizationHistory: View {
-    var pagesPerMonthList = PagesPerMonth.list
-    var body: some View {
-        VStack{
-            Text("Pages Memorized per Month")
-                .multilineTextAlignment(.center)
-                .font(.headline)
-                .fontWeight(.bold)
-                .frame(alignment: .center)
-            
-            ScrollView(.horizontal) {
-                let maxPagesMemorized = pagesPerMonthList.max(by: { $1.pagesMemorized > $0.pagesMemorized}) ?? PagesPerMonth(date: Date(), pagesMemorized: 1)
-                let maxYAxisValue = (max(maxPagesMemorized.pagesMemorized,Int(PagesPerMonth.overall)) * 2)
-                Chart {
-                    ForEach(pagesPerMonthList) { pagesPerMonth in
-                        BarMark(
-                            x: .value("Week", pagesPerMonth.date, unit: .month),
-                            y: .value("Pages", pagesPerMonth.pagesMemorized)
-                        )
-                        .foregroundStyle(.foreground)
-                    }
-                    
-                    
-                    RuleMark(y:.value("Average", PagesPerMonth.overall))
-                        .foregroundStyle(Color.red)
-                        .lineStyle(StrokeStyle(dash: [5]))
-                        .annotation(alignment: .leading){
-                            (
-                                Text("Avg: ")
-                                + Text(NumberFormatter.twoDecimals(PagesPerMonth.overall) ?? "0.00")
-                            )
-                            .font(.caption)
-                            .fontWeight(.bold)
-                            .foregroundColor(.red).opacity(0.9)
-                        }
-                    
-                }
-                .padding()
-                .frame(minWidth: 200, minHeight: 200)
-                .chartYScale(domain: 0...maxYAxisValue)
-                .chartXScale(domain: (pagesPerMonthList.min(by: {$1.date>$0.date})?.date ?? Date())...Date.now)
-                .chartXAxis {
-                    AxisMarks( values: pagesPerMonthList.map{ $0.date }){ _ in
-                        AxisValueLabel(format: .dateTime.month(.narrow))
-                        AxisTick()
-                        AxisGridLine()
-                    }
-                }
-                .scaledToFill()
-            }.frame(maxWidth: 1000)
-                .scaledToFill()
-        }
-    }
-}
-
