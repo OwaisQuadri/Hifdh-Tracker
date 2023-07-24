db17ef92c6b370e2d234a9b8b370d2901cf2cbf4
Owais Quadri <owaisquadri01@gmail.com>
2023-07-23 21:23:10 -0400

    #18 / #19 : - Create Constants for Default names and Errors

---
+++ Hifdh Tracker/Util/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Hifdh Tracker
+//
+//  Created by Owais on 2023-07-23.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let dateGoalDefaultName = "Date Goal"
+    static let pagesPerIntervalGoalDefaultName = "Pages per Interval Goal"
+    static let endPageGoalDefaultName = "End Page Goal"
+    
+    static let genericTextFieldEmptyError = "One or more text fields are empty!"
+}
